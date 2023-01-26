d04841b52dcfe019ebab82a2bb3ec953258daec5
smukhitov <smukhitov@gmail.com>
2023-01-26 15:50:53 -0500

    Initial commit

---
+++ src/test/java/com/smukhitov/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smukhitov.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
